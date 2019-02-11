a652f1c4ab59a8deb34c06404ae9c35a2ff501ee
kuroda-kentaro <kuroda_business@outlook.jp>
2019-02-11 19:25:30 +0900

    Add_Allowed_Host_Heroku

--- onlineschool/onlineschool/settings.py
+++ onlineschool/onlineschool/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '172.19.0.3', '[::1]']
 ALLOWED_HOSTS = ['localhost', '0.0.0.0', '[::1]']
+ALLOWED_HOSTS = ['web-system.herokuapp.com']
 
 
 # Application definition
