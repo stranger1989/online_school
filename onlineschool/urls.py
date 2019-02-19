e4c63736acb2c4edea7437ca0644aaf5ce5a0ca9
kuroda-kentaro <kuroda_business@outlook.jp>
2019-02-19 15:49:17 +0900

    Create_User_Lesson_LessonRecord_Url

--- onlineschool/urls.py
+++ onlineschool/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from . import views
 
+app_name = 'onlineschool'
+
 urlpatterns = [
-    path("", views.aisatsu, name= "aisatsu"),
-    path("form",views.form, name="form"),
+    path('', views.index, name='index'),
+    path('user_list', views.user_list, name='user_list'),
+    path('user_form', views.user_form, name='user_form'),
+    path('user_form/<int:user_id>/', views.user_edit, name='user_edit'),
+    path('user_delete/<int:user_id>/', views.user_delete, name='user_delete'),
+    path('lesson_list', views.lesson_list, name='lesson_list'),
+    path('lesson_form', views.lesson_form, name='lesson_form'),
+    path('lesson_form/<int:lesson_id>/', views.lesson_edit, name='lesson_edit'),
+    path('lesson_delete/<int:lesson_id>/', views.lesson_delete, name='lesson_delete'),
 ]
