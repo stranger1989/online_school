7fa7e6defdcd818e16ce2e66ca5919ef6ca0ee1a
kuroda-kentaro <kuroda_business@outlook.jp>
2019-02-19 15:48:22 +0900

    Create_User_LessonRecord_Model

--- onlineschool/form.py
+++ onlineschool/form.py
@@ -1,6 +1,33 @@
 from django import forms
 
-class aisatsuform(forms.Form):
-    name = forms.CharField(label="name")
-    area = forms.CharField(label="area")
-    age = forms.IntegerField(label="age")
+from onlineschool.models import User
+from onlineschool.models import LessonRecord
+
+
+class UserForm(forms.ModelForm):
+    sex = forms.ChoiceField(choices=User.SEX_CHOICES, label='性別')
+
+    class Meta:
+        model = User
+        fields = ('name', 'sex', 'age',)
+        labels = {
+            'name': '名前',
+            'sex': '性別',
+            'age': '年齢'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class LessonRecordForm(forms.ModelForm):
+
+    class Meta:
+        model = LessonRecord
+        fields = ('user_name', 'lesson_name', 'lesson_date', 'lesson_hour')
+        labels = {
+            'user_name': '顧客名',
+            'lesson_name': 'ジャンル',
+            'lesson_date': '受講日',
+            'lesson_hour': '受講時間(h)',
+        }
