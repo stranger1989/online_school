78aa9fe956f47671061f61e33e091136e3a8cd7a
kuroda-kentaro <kuroda_business@outlook.jp>
2019-02-19 15:46:19 +0900

    Create_Admin_function

--- onlineschool/admin.py
+++ onlineschool/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Lesson, LessonRecord
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Lesson)
+admin.site.register(LessonRecord)
